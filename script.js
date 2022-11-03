a86b0b520033bf9e102acc0f03d338887edfd7f3
d4niell <67240995+d4niell@users.noreply.github.com>
2022-11-03 18:16:17 +0200

    more codenz

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 window.onscroll = function() {myFunction()};
 var header = document.getElementById("myHeader");
 var sticky = header.offsetTop;
+const button = document.getElementById("btn");
+
+button.addEventListener("click", ()=>{
+  var search_input = document.getElementById("search_input").value;
+  if (!search_input == "") {
+    console.log(search_input);   
+  }
+  else
+  alert("Empty field.");
+
+})
+
 
 
 function myFunction() {
