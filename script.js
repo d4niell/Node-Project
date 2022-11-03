7de007ef299b9f1ddbb84e64363131d6e4f56816
d4niell <67240995+d4niell@users.noreply.github.com>
2022-11-03 18:52:54 +0200

    style update

--- script.js
+++ script.js
@@ -2,11 +2,10 @@ window.onscroll = function() {myFunction()};
 var header = document.getElementById("myHeader");
 var sticky = header.offsetTop;
 const button = document.getElementById("btn");
-
 button.addEventListener("click", ()=>{
   var search_input = document.getElementById("search_input").value;
   if (!search_input == "") {
-    console.log(search_input);   
+  //TODO
   }
   else
   alert("Empty field.");
